848e61e9d16e73628b0f37c5fcad16fee3d996e8
Léo Moscillo <leo.moscillo@anthropi.fr>
2022-08-12 16:27:24 +0200

    Updating menu a bit

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  menuState = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openOrCloseMenu() {
+    this.menuState = !this.menuState;
   }
 
 }
